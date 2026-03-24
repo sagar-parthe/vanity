b202ff174076a1ac2c866ded32a7b0ae738eb8f7
Sagar Parthe <sagar@Sagars-MacBook-Pro.local>
2026-03-24 23:38:14 +0530

    boilerplate shadcn setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from "react-dom/client"
 
 import "./index.css"
 import App from "./App.tsx"
+import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { TooltipProvider } from "@/components/ui/tooltip"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
+    </ThemeProvider>
   </StrictMode>
 )
